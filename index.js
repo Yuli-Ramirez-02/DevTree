ab14e5d9ed24be863c0ebcf24554858d17e1df71
unknown <ramirezyuli412@gmail.com>
2025-03-03 20:24:57 -0500

    creating a script

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo desde DevTree');
+const express = require('express')
+
+const app = express()
+
+//Routing
+app.get('/', (req, res) => {
+    res.send('Hola mundo en Express')
+})
+
+app.listen(4000, () => {
+    console.log('Servidor Funcionando....');
+})
